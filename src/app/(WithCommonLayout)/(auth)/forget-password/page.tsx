1c32217ec104c5c56723091623a9494e9bc2cc07
rahulhasan31 <rahulhasan666@gmail.com>
2024-12-11 01:31:25 +0600

    updated again

--- src/app/(WithCommonLayout)/(auth)/forget-password/page.tsx
+++ src/app/(WithCommonLayout)/(auth)/forget-password/page.tsx
@@ -5,37 +5,33 @@ import CommonAuthImage from "@/components/Ui/CommonAuthImage";
 
 import { Button } from "@nextui-org/button";
 import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function ForgetPassword() {
   const router = useRouter();
-  //this is set new password route if you need next step please just enter your email and click forget passaword button
-  const onSubmitHandle = (e: any) => {
+
+  // This function handles form submission
+  const onSubmitHandle = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (e) {
-      router.push("/set-new-password");
-    }
+    router.push("/set-new-password");
   };
+
   return (
     <>
       <CommonAuthImage />
 
-      <div
-        // style={{
-        //   backgroundColor: " #f8fafc",
-        // }}
-        className="lg:flex w-full h-screen mx-auto lg:flex-col items-center"
-      >
-        <div className="lg:w-[50%] bg-gradient-to-bl from-default-100   max-sm:mx-auto p-5 shadow-lg rounded-lg mt-4 max-md:[50%]">
-          <div className="text-center text-3xl font-bold mb-2">
-            Forgot password
+      <div className="flex w-full h-screen mx-auto flex-col items-center lg:flex-row">
+        <div className="lg:w-[50%] bg-gradient-to-bl from-default-100 max-sm:mx-auto p-5 shadow-lg rounded-lg mt-4 max-md:w-[80%]">
+          <div className="text-center text-3xl font-bold mb-4">
+            Forgot Password
           </div>
 
-          <form onSubmit={onSubmitHandle}>
-            <FXInput className="lg:w-full mb-5" name="email" label="Email" />
+          <form onSubmit={onSubmitHandle} className="w-full">
+            <FXInput className="w-full mb-5" name="email" label="Email" />
 
             <Button
               type="submit"
-              className="w-full  mt-2 bg-gradient-to-tr  from-orange-500 to-orange-500 text-white shadow-lg"
+              className="w-full mt-2 bg-gradient-to-tr from-orange-500 to-orange-500 text-white shadow-lg"
               radius="full"
             >
               Forget Password
